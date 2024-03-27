27031eb66baae8ad4eff3c9e12a42ee1752e50c6
nastyamalin <98037798+nastyamalin@users.noreply.github.com>
2024-03-27 11:53:15 +0200

    Add files via upload

---
+++ lab_3/lab_3/Class1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+class Lab_3
+{
+    private int minValue;
+    private int maxValue;
+    private int value;
+    public Lab_3()
+    {
+        minValue = 0;
+        maxValue = 100;
+        value = minValue;
+    }
+    public Lab_3(int min, int max, int initialValue)
+    {
+
+        Console.Write("Мінімум ");
+        minValue = int.Parse(Console.ReadLine());
+
+        Console.Write("Максимум");
+        maxValue = int.Parse(Console.ReadLine());
+
+        Console.Write("Поточне число");
+        initialValue = int.Parse(Console.ReadLine());
+
+        if (initialValue >= minValue && initialValue <= maxValue)
+            value = initialValue;
+        else
+            value = minValue;
+
+    }
+    public void Increment()
+    {
+        if (value < maxValue)
+            value++;
+        else
+            Console.WriteLine("=max");
+    }
+    public void Decrement()
+    {
+        if (value > minValue)
+            value--;
+        else
+            Console.WriteLine("=min");
+    }
+
+    public int Value
+    {
+        get { return value; }
+    }
+    public int CurrentValue { get; private set; }
+
+    public void SetCurrentValue(int value)
+    {
+        if (value >= minValue && value <= maxValue)
+            CurrentValue = value;
+        else
+            CurrentValue = minValue;
+    }
+    public void SaveToJson(string fileName)
+    {
+        string path = @"C:\Users\Asus\Desktop\uni\lab_3\lab_3\bin\Debug\net6.0\" + fileName + ".json";
+        string json = JsonConvert.SerializeObject(this);
+        File.WriteAllText(path, json);
+        Console.WriteLine("збережено у" + path);
+    }
+    public static Lab_3 LoadFromJson(string fileName)
+    {
+        string path = @"C:\Users\Asus\Desktop\uni\lab_3\lab_3\bin\Debug\net6.0\" + fileName + ".json";
+        string json = File.ReadAllText(path);
+        var obj = JsonConvert.DeserializeObject<Lab_3>(json);
+
+        Lab_3 counter = new Lab_3();
+        counter.SetCurrentValue(obj.Value);
+
+        return counter;
+    }
+}
---
+++ lab_3/lab_3/Program.cs
@@ -0,0 +1,77 @@
+﻿//Описати клас, який реалізує десятковий лічильник, який може збільшувати або зменшувати своє значення на одиницю в заданому діапазоні.
+//Передбачити ініціалізацію лічильника значеннями за замовчуванням і довільними значеннями. Лічильник має два методи: збільшення і зменшення та властивість,
+//що дозволяє отримати його поточний стан.
+//Написати програму, яка демонструвала б всі можливості класу. Зробити властивості класу приватними, а для їх читання створити методи-геттери.
+
+
+using System;
+using System.Diagnostics.Metrics;
+using Newtonsoft.Json;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Лічильник за замовчуванням,з json файлу чи самостійно створений(0<x<100) ('1' або '2' або '3')");
+        string choice = Console.ReadLine().ToLower();
+        Lab_3 counter;
+        if (choice == "1")
+        {
+            counter = new Lab_3();
+        }
+        else if (choice == "2")
+        {
+            Console.Write("Введіть ім'я JSON файлу: ");
+            
+            string fileNameLoad = Console.ReadLine();
+            counter = Lab_3.LoadFromJson(fileNameLoad);
+            Console.WriteLine("Дані завантажено з файлу: " + fileNameLoad);
+        }
+        else if (choice == "3")
+        {
+            counter = new Lab_3(0, 0, 0);
+        }
+        else
+        {
+            Console.WriteLine("Помилка");
+            return;
+        }
+
+
+        while (true)
+        {
+            Console.WriteLine("\nОберіть дію:");
+            Console.WriteLine("1. Збільшити");
+            Console.WriteLine("2. Зменшити");
+            Console.WriteLine("3. Поточний стан");
+            Console.WriteLine("4. Зберегти в JSON");
+            Console.WriteLine("5. Вийти");
+
+            int choice_2 = int.Parse(Console.ReadLine());
+
+            switch (choice_2)
+            {
+                case 1:
+                    counter.Increment();
+                    break;
+                case 2:
+                    counter.Decrement();
+                    break;
+                case 3:
+                    Console.WriteLine("Поточний стан: " + counter.Value);
+                    break;
+                case 4:
+                    Console.Write("Ім'я файлу");
+                    string fileNameSave = Console.ReadLine();
+                    counter.SaveToJson(fileNameSave);
+                    break;
+                case 5:
+                    Console.WriteLine("Завершено роботу програми");
+                    return;
+                default:
+                    Console.WriteLine("Помилка");
+                    break;
+            }
+        }
+
+    }
+}
